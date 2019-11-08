78092d70357f3ee0075cf2c3688c92cd910ec521
yashshah4e <yashshah4@gmail.coml>
2019-11-08 18:21:50 +0100

    A small sample of Tic tac toe game added

--- charactercount.py
+++ charactercount.py
@@ -1,6 +1,15 @@
+#The following module helps prettify the printout of a dictionary
+#This module includes pprint() & pformat() to improve what print() generally offers
+import pprint
+#Asking for a text input from the user and saving it as a string
 message = str(raw_input("Enter a text : "))
+#Declaring a dictionary to count characters
 count={}
+#iterating through each character
 for chara in message:
     count.setdefault(chara,0)
     count[chara]= count[chara]+1
-print (str(count))
+pprint.pprint (str(count))
+#If you want to obtain the prettified text as a string value instead of
+#dis- playing it on the screen, call pprint.pformat() instead
+# pprint.pformat(count)
